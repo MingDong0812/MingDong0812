318979d7b344143bb71bdd0c8ce94c965b0366fb
MingDong0812 <129807069+MingDong0812@users.noreply.github.com>
2023-07-02 21:51:21 +0900

    alert 수정

--- js/scripts.js
+++ js/scripts.js
@@ -113,7 +113,7 @@ function copy_to_clipboard(event) {
     copyText.select();
     copyText.setSelectionRange(0, 99999);
     document.execCommand("Copy");
-    alert('감삼다 감삼다');
+    alert('감사합니다.');
 };
 
 function fn_audio() {
